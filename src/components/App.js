0f74c1a1cc86d8989552d384717a25c5033979ea
qiannyqian <liang.huiqian@gmail.com>
2020-03-25 11:53:29 +0800

    Added axios, integrated youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+const KEY = 'AIzaSyAa-M2M9hbbEZLvuu20hqeISW7IBhSxXzY';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have{' '}
+        {this.state.videos.length} videos.
       </div>
     );
   }
